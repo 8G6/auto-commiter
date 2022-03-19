301d6acd6afab2d11a6780a6e35bf46881084171
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-19 19:39:38 +0530

    auto-update-auto_commit.js

--- auto_commit.js
+++ auto_commit.js
@@ -29,7 +29,7 @@ async function run(){
     let out=''
     for(i=0;i<arr.length;i++){
         if(arr[i].split('.').length==1){
-            let {stdout} = await exe(`cd ${dir} cd ${arr[i]} && dir /b/s`)
+            let {stdout} = await exe(`cd ${dir} && cd ${arr[i]} && dir /b/s`)
             out+=stdout.split('\n').filter(n=>n).join('\n')
             await exe('cd ..')
         }
