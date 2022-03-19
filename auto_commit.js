6a23ce0d15e2bdd0a5ffe8113b4f3c376ac7a41c
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-19 19:54:21 +0530

    global

--- auto_commit.js
+++ auto_commit.js
@@ -53,7 +53,7 @@ async function run(){
                 n=n.split('\\')
                }
               catch(e){}
-                console.log(`uprepo auto-update-${n[n.length-1].length!=1 ? n[n.length-1]: n}(${c})`)
+                console.log(`uprepo auto-update-${n[n.length-1].length!=1 ? n[n.length-1]: n}(${curr.mtime.split(' ')[4]})`)
                 const {stdout} = await exe(`uprepo auto-update-${n[n.length-1].length!=1 ? n[n.length-1]: n}(${c})`)
                 console.log(stdout)
             }
@@ -64,7 +64,7 @@ async function run(){
         });
         
     })
-    c++
+    
 
 }
 
